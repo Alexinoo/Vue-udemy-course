f67cf25540f15332d66fb559c2ec5255d4a5cec8
Alex Mwangi <alex@stl-horizon.com>
2022-05-27 15:09:29 +0300

    createApp() takes in an oject with data property which takes in a function that MUST return an OBJECT;Property of the object can store anything ; [], {},boolean or string; In this case, we define courseGoal property that stores a string

--- 03-Basics-core-concepts/app.js
+++ 03-Basics-core-concepts/app.js
@@ -1,7 +1,24 @@
 
 const app = Vue.createApp({
+
+    // Has to be data - Takes in a function that Must return an object NOT an Array/String/Number
+    data : ()=>{
+
+        return {
+
+            courseGoal : 'Finish the course'
+
+         // Property courseGoal Can store anything ; [array] ,boolean(true) , string , number
+
+         // courseGoal : string
+         // courseGoal : integer
+         // courseGoal : []
+         // courseGoal : {}
+
+        }
+    }
+
  
 })
-
 
 app.mount('#user-goal')
