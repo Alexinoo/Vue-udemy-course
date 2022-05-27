33c6c5dbe587d275ceaec755f9e3626deb6fd30b
Alex Mwangi <alex@stl-horizon.com>
2022-05-27 16:09:30 +0300

    Added methods property that takes in an object not a () - Then defines methods/properties that takes in functions that can perfom some operations/react to some user events

--- 03-Basics-core-concepts/app.js
+++ 03-Basics-core-concepts/app.js
@@ -16,6 +16,22 @@ const app = Vue.createApp({
          // courseGoal : {}
 
         }
+    } ,
+
+    // Methods : Takes in an object with user defined functions which aree callable to do something ; i.e either react to user events or perfoma calculations
+
+    methods : {
+
+        outputGoal : ()=>{
+
+            const randomNumber = Math.random()
+
+            if( randomNumber < 0.5 ){
+                return 'Learn Vue !'
+            }else{
+                return 'Master Vue !'
+            }
+        }
     }
 
  
