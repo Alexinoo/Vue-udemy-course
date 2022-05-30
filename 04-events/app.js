0fad957a330f1b24608d99d7f8725ba824eddb0a
Alex Mwangi <alex@stl-horizon.com>
2022-05-30 12:03:24 +0300

    Events and methods

--- 04-events/app.js
+++ 04-events/app.js
@@ -4,6 +4,18 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+
+  methods : {
+
+    // No need to return anything - 
+    increment(){
+      this.counter = this.counter + 1
+    } ,
+
+    decrement(){
+      this.counter = this.counter - 1
+    } ,
+  }
 });
 
 app.mount('#events');
