38a337ad6cab63fbcf27eece09cb3a599d033b76
Alex Mwangi <alex@stl-horizon.com>
2022-05-30 14:27:55 +0300

    Locking content with v-once Maintains initial state before the value changes

---
+++ 08-locking-content/app.js
@@ -0,0 +1,40 @@
+
+
+const app = Vue.createApp({
+
+    data() {
+
+        return {
+            counter: 0,
+            name: '',
+            confirmedName: '',
+        }
+
+    },
+    methods: {
+
+        confirmInput() {
+            this.confirmedName = this.name
+        },
+
+        setName(event, lastName) {
+            this.name = event.target.value + ' ' + lastName
+        },
+
+        submitForm() {
+            console.log('Submitted!');
+
+        },
+
+        increment(num) {
+            this.counter = this.counter + num
+        },
+
+        decrement(num) {
+            this.counter = this.counter - num
+        },
+    }
+
+})
+
+app.mount('#events')
