04afbb6925f1db6bfa729eb8c2372b5cc0a6d1ea
Alex Mwangi <alex@stl-horizon.com>
2022-06-01 21:44:21 +0300

    Rendering lists of data with v-for

---
+++ 20-Rendering-lists-of-data-using-v-for/app.js
@@ -0,0 +1,18 @@
+
+
+const app = Vue.createApp({
+  data() {
+    return {
+      enteredValue : '',
+       goals: []
+       }
+  },
+  methods : {
+
+    addGoal(){
+      this.goals.push(this.enteredValue)
+    }
+  }
+})
+
+app.mount('#user-goals')
